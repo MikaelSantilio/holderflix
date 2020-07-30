cc821b8f865a3b8d6d4d4fafed6d900446e81cdf
MikaelSantilio <mikael.santilio@gmail.com>
2020-07-30 14:34:34 -0300

    Adding react-routes

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import './Menu.css';
 import Button from '../Button';
+import {Link} from 'react-router-dom';
 // import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/" >
+            <Link to="/" >
                 <img className="logo" src={logo} alt="Holderflix" ></img>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/" >
+            <Button as={Link} className="ButtonLink" to="/cadastro/video" >
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+import Video from './pages/cadastro/Video';
+import Categoria from './pages/cadastro/Categoria';
+
+
+
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path="/cadastro/video" component={Video} exact />
+      <Route path="/cadastro/categoria" component={Categoria} exact />
+      <Route path="/" component={Home} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </Router>,
+
   document.getElementById('root')
 );
